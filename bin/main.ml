b5526af44aac854a3b612138d736e26ad92f57ca
ZebHunter <misharoga4ew@gmail.com>
2024-09-26 23:52:50 +0300

    first commit

---
+++ bin/main.ml
@@ -0,0 +1,16 @@
+let () = 
+  Printf.printf "key: %s\n" (List.fold_left (fun acc x -> acc ^ x) "" Lab1.Polibiy.create_key);
+  print_endline "Введите имя файла:";
+  let filename = read_line () in 
+  let content = Lab1.Polibiy.read_file filename in
+  content 
+  |> Lab1.Polibiy.encode_text Lab1.Polibiy.create_key 
+  |> Lab1.Polibiy.write_text "out.txt";
+  print_endline "...Encoded...";
+  let content = Lab1.Polibiy.read_file "out.txt" in
+  content 
+  |> Lab1.Polibiy.decode_text Lab1.Polibiy.create_key 
+  |> Lab1.Polibiy.write_text "decoded.txt";
+  print_endline "...Decoded";
+
+  
---
+++ lib/polibiy.ml
@@ -0,0 +1,74 @@
+let rec ( -- ) a b = 
+  match compare a b with
+  | 1 -> []
+  | _ -> a :: (Uchar.succ a -- b)
+
+let decode_uchar u = let b = Buffer.create 4 in 
+  Buffer.add_utf_8_uchar b u;
+  Buffer.contents b
+
+let map_uchar_to_string l = List.map (fun x -> decode_uchar x) l
+
+let shuffle d =
+  let nd = List.map (fun c -> (Random.bits (), c)) d in
+  let sond = List.sort compare nd in
+  List.map snd sond
+
+let polibiy_index e l = 
+  let i = List.find_index (fun x -> x = e) l in
+  match i with
+  | Some i -> (i / 9 , i mod 9)
+  | None -> (-1, -1)
+
+let el_by_index (a, b) l = 
+  List.nth l (a * 9 + b)
+
+let write_text filename ul =
+  let oc = open_out filename in
+  output_string oc ul;
+  close_out oc
+
+let read_file filename =
+  let ic = open_in filename in
+  let rec read_lines acc =
+    try
+      let line = input_line ic in
+      read_lines (line :: acc)
+    with End_of_file ->
+      close_in ic;
+      List.rev acc
+  in
+  String.concat "\n" (read_lines [])
+
+let chars_to_string cl = let b = Buffer.create 16 in 
+  List.iter (Buffer.add_char b) cl;
+  Buffer.contents b 
+
+let encode_text key str =
+  let s = List.init (String.length str) (String.get str) in
+  let rec encode s key acc = 
+    match s with
+    | [] -> List.rev acc
+    | x1 :: x2 :: xs when x1 = '\208' || x1 = '\209' -> let (a, b) = polibiy_index (chars_to_string (x1 :: x2 :: [])) key in
+                                      encode xs key (b :: a :: acc)
+    | h :: t -> let (a, b) = polibiy_index (String.make 1 h) key in
+                encode t key (b :: a :: acc)
+  in
+  encode s key [] |> List.fold_left (fun acc1 x -> acc1 ^ string_of_int x) ""
+
+let decode_text key str = 
+  let s = List.init (String.length str) (String.get str) in
+  let i = List.map (fun x -> (int_of_char x) - 48) s in
+  let rec decode i key acc =
+    match i with
+    | [] -> List.rev acc
+    | x1 :: x2 :: xs -> let el = el_by_index (x1, x2) key in
+                        decode xs key (el :: acc)
+    | _ :: [] -> raise (Failure "Ошибка декодирования")
+  in 
+  decode i key [] |> List.fold_left (fun acc1 x -> acc1 ^ x) ""
+
+let create_key = (Uchar.of_int 1040) -- (Uchar.of_int 1103) 
+  |> map_uchar_to_string 
+  |> List.append [" "; ";"; "."; ","; "-"; "\n"; "\t"; "?"; "!"; "ё"; ":"; "\""; "("; ")"]
+  |> shuffle 
