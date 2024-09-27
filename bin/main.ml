18f0414756e4c7437df9e6c5278db1b94f7ad70e
ZebHunter <misharoga4ew@gmail.com>
2024-09-27 12:34:58 +0300

    add comments and new shuffle strategy

--- bin/main.ml
+++ bin/main.ml
@@ -1,18 +1,25 @@
+(*
+  Точка входа в программу
+*)
 
 let () = 
-  Printf.printf "key: %s\n" (List.fold_left (fun acc x -> acc ^ x) "" Lab1.Polibiy.create_key);
   print_endline "Введите имя файла:";
   let filename = read_line () in 
-  let content = Lab1.Polibiy.read_file filename in
-  content 
-  |> Lab1.Polibiy.encode_text Lab1.Polibiy.create_key 
-  |> Lab1.Polibiy.write_text "out.txt";
+    let content = Lab1.Polibiy.read_file filename in
+      let key = Lab1.Polibiy.create_key (String.length content) in
+      let print_key = Lab1.Polibiy.write_text "key.txt" (List.fold_left (fun acc x -> acc ^ x) "" key) in
+      print_key;
+      Printf.printf "\n%s\n" (Lab1.Polibiy.read_file filename);
+        content 
+        |> Lab1.Polibiy.encode_text key 
+        |> Lab1.Polibiy.write_text "out.txt";
   print_endline "\n...Encoded...";
   Printf.printf "\n%s\n" (Lab1.Polibiy.read_file "out.txt");
   let content = Lab1.Polibiy.read_file "out.txt" in
-  content 
-  |> Lab1.Polibiy.decode_text Lab1.Polibiy.create_key 
-  |> Lab1.Polibiy.write_text "decoded.txt";
+    let key = Lab1.Polibiy.read_key "key.txt" in
+      content 
+      |> Lab1.Polibiy.decode_text key
+      |> Lab1.Polibiy.write_text "decoded.txt";
   print_endline "\n...Decoded";
   Printf.printf "\n%s\n" (Lab1.Polibiy.read_file "decoded.txt");
 
--- lib/polibiy.ml
+++ lib/polibiy.ml
@@ -1,26 +1,66 @@
+(* 
+  Служебная функция, предоставляющая инфиксный оператор 
+  для записи диапазонов широких кодировок 
+
+  val ( -- ): Uchar.t -> Uchar.t -> Uchar.t list = <fun>
+*)
+
 let rec ( -- ) a b = 
   match compare a b with
   | 1 -> []
   | _ -> a :: (Uchar.succ a -- b)
 
+(*
+  Функция для преобразования кодировки в строку
+
+  val decode_uchar: Uchar.t -> string = <fun>
+*)
+
 let decode_uchar u = let b = Buffer.create 4 in 
   Buffer.add_utf_8_uchar b u;
   Buffer.contents b
 
+(*
+  Преобразование списка Uchar.t к строковому типу
+  Данная функция необходима для обеспечения целосотности работы с русским алфавитом
+
+  val map_uchar_to_string: Uchar.t list -> string list = <fun>
+*)
+
 let map_uchar_to_string l = List.map (fun x -> decode_uchar x) l
+
+(*
+  Функция перемешивания списка
+  Нужна для обеспечения рандомности квадрата Полибия
+
+  val shuffle: a' list -> a' list = <fun>
+*)
 
 let shuffle list =
   let shuffled = List.map (fun x -> (Random.bits(), x)) list in
   let sorted = List.sort compare shuffled in
   List.map snd sorted
 
-let shuffle_random_times list =
-  let reshuffle_count = Random.int 10 + 1 in
+(*
+  Функция для обеспечения большей степени рандомизации квадрата,
+  т.к. элементы квадрата статичны
+
+  val shuffle_random_times: int -> a' list -> a' list = <fun>
+*)
+
+let shuffle_random_times n list =
+  let reshuffle_count = Random.int n + 1 in
   let rec apply_shuffle n lst =
     if n <= 0 then lst
     else apply_shuffle (n - 1) (shuffle lst)
   in
   apply_shuffle reshuffle_count list
+
+(*
+  Функция получения кода символа из квадрата Полибия
+
+  val polibiy_index: a' -> a' list -> int * int = <fun>
+*)
 
 let polibiy_index e l = 
   let i = List.find_index (fun x -> x = e) l in
@@ -28,13 +68,31 @@ let polibiy_index e l =
   | Some i -> (i / 9 , i mod 9)
   | None -> (-1, -1)
 
+(*
+  Функция получения символа из квадрата Полибия по индексу
+
+  val el_by_index: int * int -> a' list -> a' = <fun>
+*)
+
 let el_by_index (a, b) l = 
   List.nth l (a * 9 + b)
+
+(*
+  Функция записи в файл
+
+  val write_text: string -> string -> unit = <fun>
+*)
 
 let write_text filename ul =
   let oc = open_out filename in
   output_string oc ul;
   close_out oc
+
+(*
+  Функция чтения из файла
+
+  val read_file: string -> string = <fun>
+*)
 
 let read_file filename =
   let ic = open_in filename in
@@ -48,9 +106,21 @@ let read_file filename =
   in
   String.concat "\n" (read_lines [])
 
+(*
+  Функция перевода списка char к строке
+
+  val chars_to_string: char list -> string = <fun>
+*)
+
 let chars_to_string cl = let b = Buffer.create 16 in 
   List.iter (Buffer.add_char b) cl;
   Buffer.contents b 
+
+(*
+  Функция кодирования текста с рандомным квадратом Полибия
+
+  val encode_text: string list -> string -> string = <fun>
+*)
 
 let encode_text key str =
   let s = List.init (String.length str) (String.get str) in
@@ -64,6 +134,12 @@ let encode_text key str =
   in
   encode s key [] |> List.fold_left (fun acc1 x -> acc1 ^ string_of_int x) ""
 
+(*
+  Функция декодирования текста с рандомным квадратом Полибия
+
+  val decode_text: string list -> string -> string = <fun>
+*)
+
 let decode_text key str = 
   let s = List.init (String.length str) (String.get str) in
   let i = List.map (fun x -> (int_of_char x) - 48) s in
@@ -76,7 +152,30 @@ let decode_text key str =
   in 
   decode i key [] |> List.fold_left (fun acc1 x -> acc1 ^ x) ""
 
-let create_key = (Uchar.of_int 1040) -- (Uchar.of_int 1103) 
+(*
+  Функция создания ключа с параметром от длины текста
+
+  val create_key: int -> string list = <fun>
+*)
+
+let create_key n = (Uchar.of_int 1040) -- (Uchar.of_int 1103) 
   |> map_uchar_to_string 
   |> List.append [" "; ";"; "."; ","; "-"; "\n"; "\t"; "?"; "!"; "ё"; ":"; "\""; "("; ")"]
-  |> shuffle_random_times 
+  |> shuffle_random_times n
+
+(*
+  Функция чтения квадрата Полибия из файла (нужна для декодирования)
+
+  val read_key: string -> string list = <fun>
+*)
+
+let read_key filename =
+  let k = read_file filename in
+  let key = List.init (String.length k) (String.get k) in
+  let rec key_to_list key acc =
+    match key with
+    | [] -> List.rev acc
+    | x1 :: x2 :: xs when x1 = '\208' || x1 = '\209' -> key_to_list xs ( (chars_to_string (x1 :: x2 :: [])) :: acc )
+    | h :: t -> key_to_list t ((chars_to_string (h :: [])) :: acc)
+  in
+  key_to_list key []
